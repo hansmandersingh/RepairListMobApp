0b9bfd22ae2b6ed870db02325c907e8e575c7bc3
Hans <hansmandersingh1998@gmail.com>
2020-05-31 13:44:05 -0500

    Ported till form and got value but not updating array

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, SafeAreaView, TextInput } from 'react-native';
 
 export default class App extends React.Component {
   id = 0;
@@ -9,16 +9,37 @@ export default class App extends React.Component {
     todos: []
   }
 
-  print = () => {
-   console.log('works');
-  }
+  changingVal = (text) => {
+    this.setState({ inputText: text });
+  };
+
+  createTodo = (val) => {
+    this.setState((currentState) => ({
+      todos: [
+        ...currentState.todos,
+        { value: val, id: this.id++, checked: "" },
+      ],
+    }));
+  };
+
+  submittionUpdate = (e) => {
+    e.preventDefault();
+    this.createTodo(this.state.inputText);
+    console.log(this.state.todos)
+    this.setState({ inputText: "" });
+  };
 
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Gandu  Sallaa</Text>
-        <Button title="click me" onPress={this.print}/>
-      </View>
+      <>
+        <SafeAreaView style={styles.container}>
+          <Text style={styles.text}>rep🔥irs</Text>
+          <View>
+          <TextInput style={styles.textField} value={this.state.inputText} onChangeText={(text) => this.changingVal(text)} onSubmitEditing={this.submittionUpdate}/>
+        </View>
+        </SafeAreaView>
+        
+      </>
     )
   }
 }
@@ -28,6 +49,17 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#ffe8e8"
   },
+  text: {
+    fontSize: 50,
+    color: "#af2f2f59",
+  }, 
+  textField: {
+    flex: 0.2,
+    borderWidth: 0.2,
+    width: 300,
+    marginTop: 30,
+    borderColor: "black"
+  }
 });
