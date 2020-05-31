ec1e38cead374c5ea5ea616503cf957401c278b9
Hans <hansmandersingh1998@gmail.com>
2020-05-31 02:08:24 -0500

    Some file changes and state checks

--- App.js
+++ App.js
@@ -1,12 +1,26 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+export default class App extends React.Component {
+  id = 0;
+
+  state = {
+    inputText: "",
+    todos: []
+  }
+
+  print = () => {
+   console.log('works');
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Gandu  Sallaa</Text>
+        <Button title="click me" onPress={this.print}/>
+      </View>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
